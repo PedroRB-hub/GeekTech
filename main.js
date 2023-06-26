f4d3fc4bc881be22c3ba2a7f9c2480c40cfd4a53
Pedro R.B <108886158+PedroRB-hub@users.noreply.github.com>
2023-06-26 16:39:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ ativaMenu.addEventListener('click', () => {
     ativaMenu.classList.toggle('fa-x')
     navMenu.classList.toggle('ativado')
 })
+
+const toggleBtn = document.getElementById("toggle-mode");
+const body = document.body;
+const header = document.body.header;
+
+toggleBtn.addEventListener("click", function () {
+    body.classList.toggle("dark-mode")
+    header.classList.toggle("dark-mode-header")
+})
